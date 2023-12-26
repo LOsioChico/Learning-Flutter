239365b16d1a709cb8b3ab405826849707259b55
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-26 08:24:13 -0500

    style: refactor padding and safe area usage

--- flutter_chat/lib/screens/login_screen.dart
+++ flutter_chat/lib/screens/login_screen.dart
@@ -10,138 +10,140 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          children: [
-            const Spacer(),
-            const Text(
-              'Hello, welcome back!',
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                  color: AppColors.font),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Login to continue',
-              style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: AppColors.font),
-            ),
-            const SizedBox(height: 60),
-            const AuthInput(label: 'Username'),
-            const SizedBox(height: 26),
-            const AuthInput(label: 'Password', isPassword: true),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            children: [
+              const Spacer(),
+              const Text(
+                'Hello, welcome back!',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.font),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Login to continue',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.font),
+              ),
+              const SizedBox(height: 60),
+              const AuthInput(label: 'Username'),
+              const SizedBox(height: 26),
+              const AuthInput(label: 'Password', isPassword: true),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: AppColors.font.withOpacity(0.7),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Forgot password?'),
+                ),
+              ),
+              const SizedBox(height: 40),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: 48,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: AppColors.black,
+                    backgroundColor: AppColors.primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Log In'),
+                ),
+              ),
+              const Spacer(),
+              TextButton(
                 style: TextButton.styleFrom(
                   foregroundColor: AppColors.font.withOpacity(0.7),
                 ),
+                child: const Text('Or sign in with'),
                 onPressed: () {},
-                child: const Text('Forgot password?'),
               ),
-            ),
-            const SizedBox(height: 40),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.7,
-              height: 48,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: AppColors.black,
-                  backgroundColor: AppColors.primaryColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('Log In'),
-              ),
-            ),
-            const Spacer(),
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: AppColors.font.withOpacity(0.7),
-              ),
-              child: const Text('Or sign in with'),
-              onPressed: () {},
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.7,
-              height: 48,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: AppColors.black,
-                  backgroundColor: AppColors.white,
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image(
-                      image: AssetImage('assets/images/google.png'),
-                      width: 20,
-                      height: 20,
-                    ),
-                    SizedBox(width: 8),
-                    Text('Login with Google')
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.7,
-              height: 48,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: AppColors.black,
-                  backgroundColor: AppColors.white,
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image(
-                      image: AssetImage('assets/images/facebook.png'),
-                      width: 20,
-                      height: 20,
-                    ),
-                    SizedBox(width: 8),
-                    Text('Login with Facebook'),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Don\'t have an account?',
-                  style: TextStyle(
-                    color: AppColors.font.withOpacity(0.5),
-                  ),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: AppColors.primaryColor,
-                  ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: 48,
+                child: ElevatedButton(
                   onPressed: () {},
-                  child: const Text(
-                    'Sign Up',
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      decorationColor: AppColors.primaryColor,
-                    ),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: AppColors.black,
+                    backgroundColor: AppColors.white,
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/images/google.png'),
+                        width: 20,
+                        height: 20,
+                      ),
+                      SizedBox(width: 8),
+                      Text('Login with Google')
+                    ],
                   ),
                 ),
-              ],
-            ),
-            const Spacer(),
-          ],
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: AppColors.black,
+                    backgroundColor: AppColors.white,
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/images/facebook.png'),
+                        width: 20,
+                        height: 20,
+                      ),
+                      SizedBox(width: 8),
+                      Text('Login with Facebook'),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(
+                      color: AppColors.font.withOpacity(0.5),
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: AppColors.primaryColor,
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        decorationColor: AppColors.primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+            ],
+          ),
         ),
       ),
     );
