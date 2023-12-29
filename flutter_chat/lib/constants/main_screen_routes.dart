5576e5601e9c9ceff2b23a2b214cead74b8a784c
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-29 08:32:21 -0500

    refactor: rename pages.dart to main_screen_routes.dart

--- flutter_chat/lib/constants/main_screen_routes.dart
+++ flutter_chat/lib/constants/main_screen_routes.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_chat/models/page_model.dart';
 import 'package:flutter_chat/screens/home_screen.dart';
+import 'package:flutter_chat/screens/profile_screen.dart';
 
-final List<PageModel> pages = [
-  PageModel(route: '/home', page: const HomeScreen()),
+final List<PageModel> mainScreenRoutes = [
+  PageModel(route: '/main', page: const HomeScreen()),
   PageModel(route: '/favorite', page: const Center(child: Text('Favorite'))),
   PageModel(route: '/add-post', page: const Center(child: Text('Add Post'))),
   PageModel(route: '/messages', page: const Center(child: Text('Chat'))),
-  PageModel(route: '/profile', page: const Center(child: Text('Profile'))),
+  PageModel(route: '/profile', page: const ProfileScreen())
 ];
